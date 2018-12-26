d3a52a6b9d841a7da36d8af7139fcda59877688f
Květoslav Bastard <rainbowflippy@gmail.com>
2018-12-27 00:01:38 +0100

    Add files via upload

---
+++ test/config.h
@@ -0,0 +1,62 @@
+
+/*Keycodes I guess*/
+//Cesky znaky
+#define X_PLUS KC_1
+#define X_ES KC_2
+#define X_SS KC_3
+#define X_CC KC_4
+#define X_RR KC_5
+#define X_ZZ KC_6
+#define X_YY KC_7
+#define X_AA KC_8
+#define X_II KC_9
+#define X_EE KC_0
+
+//FN
+#define X_LSFT MO(1)
+#define X_NUMB MO(3)
+#define X_CZ   MO(2)
+#define X_FN   LT(9, KC_TAB)
+#define X_SYS  MO(9)
+
+
+//Default
+#define X_MINS KC_SLSH
+#define X_CARK KC_EQL
+#define X_LBRC RALT(KC_F)
+#define X_RBRC RALT(KC_G)
+#define X_SCLN KC_GRV
+#define X_APOS LSFT(KC_BSLS)
+#define X_BSLS RALT(KC_Q)
+#define X_SLSH LSFT(KC_LBRC)
+
+//Shift US
+#define X_TILD RALT(KC_1)
+#define X_EXLM KC_DQUO
+#define X_AT   RALT(KC_V)
+#define X_HASH RALT(KC_X)
+#define X_DLR  RALT(KC_SCLN)
+#define X_PERC KC_UNDS
+#define X_CIRC RALT(KC_3) //Upravit delay
+#define X_AMPR RALT(KC_C)
+#define X_ASTR RALT(KC_SLSH)
+#define X_LPRN LSFT(KC_RBRC)
+#define X_RPRN KC_RBRC
+#define X_UNDS LSFT(KC_SLSH)
+#define X_HACK LSFT(KC_EQL)
+
+#define X_LCBR RALT(KC_B)
+#define X_RCBR RALT(KC_N)
+#define X_COLN LSFT(KC_DOT)
+#define X_DQT  LSFT(KC_SCLN)
+#define X_PIPE RALT(KC_W)
+#define X_LABK RALT(KC_COMM)
+#define X_RABK RALT(KC_DOT)
+#define X_QUES LSFT(KC_COMM)
+
+//number layer
+#define X_EQLS KC_MINS
+#define X_USHC KC_LBRC
+#define X_USKR KC_SCLN
+
+
